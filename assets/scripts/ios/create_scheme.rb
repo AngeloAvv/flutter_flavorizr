08aac65b11b57e73cc337b2d436ce1ce8a5043ed
Glenn Ruysschaert <glenn@getdriven.app>
2020-08-26 14:16:42 +0200

    Fix: Flutter expects a build configuration named Debug- or similar.

--- assets/scripts/ios/create_scheme.rb
+++ assets/scripts/ios/create_scheme.rb
@@ -12,10 +12,10 @@ project = Xcodeproj::Project.open(project_path)
 target = project.targets.first
 
 scheme = Xcodeproj::XCScheme.new
-scheme.launch_action.build_configuration = "Debug #{scheme_name}"
+scheme.launch_action.build_configuration = "Debug-#{scheme_name}"
 scheme.set_launch_target(target)
-scheme.test_action.build_configuration = "Debug #{scheme_name}"
-scheme.profile_action.build_configuration = "Release #{scheme_name}"
-scheme.analyze_action.build_configuration = "Debug #{scheme_name}"
-scheme.archive_action.build_configuration = "Release #{scheme_name}"
+scheme.test_action.build_configuration = "Debug-#{scheme_name}"
+scheme.profile_action.build_configuration = "Release-#{scheme_name}"
+scheme.analyze_action.build_configuration = "Debug-#{scheme_name}"
+scheme.archive_action.build_configuration = "Release-#{scheme_name}"
 scheme.save_as(project_path, scheme_name)
