AngeloAvv/flutter_flavorizr

commit 8c24f6e198c272d3dea371b3ff4a417d25e27981

Fix: Flutter expects a build configuration named Debug- or similar. (#21)
Co-authored-by: Glenn Ruysschaert <glenn@getdriven.app>

assets/scripts/ios/create_scheme.rb
@@ -12,10 +12,10 @@ project = Xcodeproj::Project.open(project_path)
 target = project.targets.first
 
 scheme = Xcodeproj::XCScheme.new
-scheme.launch_action.build_configuration = "Debug #{scheme_name}"
+scheme.launch_action.build_configuration = "Debug-#{scheme_name}"
 scheme.set_launch_target(target)
-scheme.test_action.build_configuration = "Debug #{scheme_name}"
-scheme.profile_action.build_configuration = "Release #{scheme_name}"
-scheme.analyze_action.build_configuration = "Debug #{scheme_name}"
-scheme.archive_action.build_configuration = "Release #{scheme_name}"
+scheme.test_action.build_configuration = "Debug-#{scheme_name}"
+scheme.profile_action.build_configuration = "Release-#{scheme_name}"
+scheme.analyze_action.build_configuration = "Debug-#{scheme_name}"
+scheme.archive_action.build_configuration = "Release-#{scheme_name}"
 scheme.save_as(project_path, scheme_name)

commit d36acb7903cc126a713631ce2c900595aa6948cb

Created new add_firebase_build_phase.rb to add a custom build phase to link the correct firebase variant to its flavor.
Code refactoring

Updates in add_build_configuration.rb should fix #33 (caused by #21 merge)

assets/scripts/ios/add_build_configuration.rb
@@ -1,26 +1,26 @@
 require 'xcodeproj'
 require 'json'
-require "base64"
+require 'base64'
 
 if ARGV.length != 6
-  puts "We need exactly six arguments"
+  puts 'We need exactly six arguments'
   exit
 end
 
 project_path = ARGV[0]
 file_path = ARGV[1]
 flavor = ARGV[2]
-bundleId = ARGV[3]
+# bundleId = ARGV[3]
 mode = ARGV[4]
 additional_build_settings = JSON.parse(Base64.decode64(ARGV[5]))
 
 project = Xcodeproj::Project.open(project_path)
 
-base_config = project.build_configuration_list.build_configurations.detect{|config| config.name == mode}
+base_config = project.build_configuration_list.build_configurations.detect { |config| config.name == mode }
 
-build_config = project.add_build_configuration("#{mode} #{flavor}", mode.downcase == "debug" ? :debug : :release)
-build_config.base_configuration_reference = project.files.detect{|file| file.path == file_path}
-build_config.build_settings = base_config.build_settings.clone()
+build_config = project.add_build_configuration("#{mode}-#{flavor}", mode.downcase == 'debug' ? :debug : :release)
+build_config.base_configuration_reference = project.files.detect { |file| file.path == file_path }
+build_config.build_settings = base_config.build_settings.clone
 build_config.build_settings = build_config.build_settings.merge(additional_build_settings)
 
 project.save

assets/scripts/ios/add_file.rb
@@ -1,7 +1,7 @@
 require 'xcodeproj'
 
 if ARGV.length < 2 || ARGV.length > 3
-  puts "Wrong number of arguments arguments"
+  puts 'Wrong number of arguments arguments'
   exit
 end
 
@@ -10,10 +10,10 @@ file_path = ARGV[1]
 group_name = ARGV.length == 3 ? ARGV[2] : nil
 
 project = Xcodeproj::Project.open(project_path)
-group = group_name == nil ? project.main_group : project.groups.detect{|group| group.name == group_name}
+group = group_name.nil? ? project.main_group : project.groups.detect { |group| group.name == group_name }
 
 file = group.find_file_by_path(file_path)
-if file == nil
+if file.nil?
   file = group.new_reference(ARGV[1])
   project.targets[0].resources_build_phase.add_file_reference(file)
   project.save

assets/scripts/ios/add_firebase_build_phase.rb
@@ -1,0 +1,29 @@
+require 'xcodeproj'
+
+if ARGV.length != 2
+  puts 'We need exactly two arguments'
+  exit
+end
+
+project_path = ARGV[0]
+file_path = ARGV[1]
+
+content = File.read(file_path)
+
+project = Xcodeproj::Project.open(project_path)
+target = project.targets.first
+
+target.shell_script_build_phases
+      .select { |phase| phase.name == 'Firebase Setup' }
+      .each { |phase| target.build_phases.delete(phase) }
+
+phase = target.new_shell_script_build_phase('Firebase Setup')
+phase.shell_path = '/bin/sh'
+phase.shell_script = content
+phase.run_only_for_deployment_postprocessing = '0'
+phase.simple_attributes.find { |attribute| attribute.plist_name == 'outputPaths' }
+     .default_value << '$(SRCROOT)/Runner/GoogleService-Info.plist'
+
+target.build_phases.rotate!(-1)
+
+project.save

assets/scripts/ios/create_scheme.rb
@@ -1,7 +1,7 @@
 require 'xcodeproj'
 
 if ARGV.length != 2
-  puts "We need exactly two arguments"
+  puts 'We need exactly two arguments'
   exit
 end
 

lib/parser/models/flavorizr.dart
@@ -44,7 +44,7 @@ class Flavorizr {
 
   @JsonKey(
       defaultValue:
-          'https://github.com/AngeloAvv/flutter_flavorizr/releases/download/v1.0.10/assets.zip')
+          'https://github.com/AngeloAvv/flutter_flavorizr/releases/download/v1.0.11/assets.zip')
   final String assetsUrl;
 
   @JsonKey(nullable: true)

lib/parser/models/flavorizr.g.dart
@@ -26,7 +26,7 @@ Flavorizr _$FlavorizrFromJson(Map json) {
     instructions:
         (json['instructions'] as List)?.map((e) => e as String)?.toList(),
     assetsUrl: json['assetsUrl'] as String ??
-        'https://github.com/AngeloAvv/flutter_flavorizr/releases/download/v1.0.10/assets.zip',
+        'https://github.com/AngeloAvv/flutter_flavorizr/releases/download/v1.0.11/assets.zip',
     ide: _$enumDecodeNullable(_$IDEEnumMap, json['ide']),
   );
 }

lib/processors/android/android_build_gradle_processor.dart
@@ -99,7 +99,7 @@ class AndroidBuildGradleProcessor extends StringProcessor {
 
   void _appendFlavorsDimension(StringBuffer buffer) {
     buffer.writeln();
-    buffer.writeln('    ${beginFlavorDimensionsMarkup}');
+    buffer.writeln('    $beginFlavorDimensionsMarkup');
     buffer.writeln(
         '    flavorDimensions "${this._flavorizr.app.android.flavorDimensions}"');
     buffer.writeln();

lib/processors/commons/empty_file_processor.dart
@@ -23,32 +23,15 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import 'dart:io';
+import 'package:flutter_flavorizr/processors/commons/string_processor.dart';
 
-import 'package:flutter_flavorizr/exception/file_not_found_exception.dart';
-import 'package:flutter_flavorizr/exception/missing_required_fields_exception.dart';
-import 'package:flutter_flavorizr/parser/models/pubspec.dart';
-import 'package:json_annotation/json_annotation.dart';
+class EmptyFileProcessor extends StringProcessor {
 
-class Parser {
-  final String file;
+  EmptyFileProcessor() : super(input: '');
 
-  Parser({this.file});
+  @override
+  String toString() => 'EmptyFileProcessor';
 
-  Pubspec parse() {
-    File pubspecFile = File(file);
-    if (!pubspecFile.existsSync()) {
-      throw FileNotFoundException(file);
-    }
-
-    String yaml = pubspecFile.readAsStringSync();
-
-    try {
-      return Pubspec.parse(yaml);
-    } on DisallowedNullValueException catch (e) {
-      throw MissingRequiredFieldsException(e.keysWithNullValues);
-    } on MissingRequiredKeysException catch (e) {
-      throw MissingRequiredFieldsException(e.missingKeys);
-    }
-  }
+  @override
+  String execute() => '';
 }

lib/processors/google/firebase/firebase_processor.dart
@@ -24,14 +24,21 @@
  */
 
 import 'package:flutter_flavorizr/parser/models/flavors/flavor.dart';
-import 'package:flutter_flavorizr/processors/android/android_firebase_processor.dart';
+import 'package:flutter_flavorizr/processors/android/google/firebase/android_firebase_processor.dart';
 import 'package:flutter_flavorizr/processors/commons/queue_processor.dart';
-import 'package:flutter_flavorizr/processors/ios/ios_firebase_processor.dart';
+import 'package:flutter_flavorizr/processors/ios/google/firebase/ios_targets_firebase_processor.dart';
 
 class FirebaseProcessor extends QueueProcessor {
-  FirebaseProcessor(String androidDestination, String iosDestination,
-      Map<String, Flavor> flavors)
-      : super(
+  FirebaseProcessor({
+    String process,
+    String androidDestination,
+    String iosDestination,
+    String addFileScript,
+    String runnerProject,
+    String firebaseScript,
+    String generatedFirebaseScriptPath,
+    Map<String, Flavor> flavors,
+  }) : super(
           [
             ...flavors
                 .map(
@@ -48,21 +55,15 @@ class FirebaseProcessor extends QueueProcessor {
                 )
                 .values
                 .where((processor) => processor != null),
-            ...flavors
-                .map(
-                  (String flavorName, Flavor flavor) => MapEntry(
-                    flavorName,
-                    flavor.ios?.firebase != null
-                        ? IOSFirebaseProcessor(
-                            flavor.ios?.firebase?.config,
-                            iosDestination,
-                            flavorName,
-                          )
-                        : null,
-                  ),
-                )
-                .values
-                .where((processor) => processor != null),
+            IOSTargetsFirebaseProcessor(
+              process: process,
+              destination: iosDestination,
+              addFileScript: addFileScript,
+              runnerProject: runnerProject,
+              firebaseScript: firebaseScript,
+              generatedFirebaseScriptPath: generatedFirebaseScriptPath,
+              flavors: flavors,
+            ),
           ],
         );
 

lib/processors/ios/google/firebase/ios_firebase_processor.dart
@@ -29,15 +29,15 @@ import 'package:flutter_flavorizr/processors/commons/queue_processor.dart';
 
 class IOSFirebaseProcessor extends QueueProcessor {
   IOSFirebaseProcessor(
-      String source,
-      String destination,
-      String flavorName,
-      ) : super([
-    NewFolderProcessor('$destination/$flavorName'),
-    CopyFileProcessor(source, '$destination/$flavorName/GoogleService-Info.plist'),
-  ]);
+    String source,
+    String destination,
+    String flavorName,
+  ) : super([
+          NewFolderProcessor('$destination/$flavorName'),
+          CopyFileProcessor(
+              source, '$destination/$flavorName/GoogleService-Info.plist'),
+        ]);
 
   @override
   String toString() => 'IOSFirebaseProcessor';
-
 }

lib/processors/ios/google/firebase/ios_firebase_script_processor.dart
@@ -1,0 +1,67 @@
+/*
+ * Copyright (c) 2021 MyLittleSuite
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import 'package:flutter_flavorizr/processors/commons/string_processor.dart';
+
+class IOSFirebaseScriptProcessor extends StringProcessor {
+  final Iterable<String> _flavors;
+
+  IOSFirebaseScriptProcessor(
+    this._flavors, {
+    String input,
+  }) : super(input: input);
+
+  @override
+  String toString() => 'IOSFirebaseScriptProcessor';
+
+  @override
+  String execute() {
+    StringBuffer buffer = StringBuffer();
+
+    if (_flavors.isNotEmpty) {
+      final iterator = _flavors.iterator;
+      iterator.moveNext();
+
+      buffer.writeln('if ${_generateCondition(iterator.current)}');
+      buffer.writeln('  ${_generateCopy(iterator.current)}');
+
+      while (iterator.moveNext()) {
+        buffer.writeln('elif ${_generateCondition(iterator.current)}');
+        buffer.writeln('  ${_generateCopy(iterator.current)}');
+      }
+
+      buffer.writeln('fi');
+      buffer.writeln();
+    }
+
+    return buffer.toString();
+  }
+
+  String _generateCondition(String flavorName) =>
+      '[ "\$CONFIGURATION" == "Debug-$flavorName" ] || [ "\$CONFIGURATION" == "Release-$flavorName" ]; then';
+
+  String _generateCopy(String flavorName) =>
+      'cp Runner/$flavorName/GoogleService-Info.plist Runner/GoogleService-Info.plist';
+}

lib/processors/ios/google/firebase/ios_targets_firebase_processor.dart
@@ -1,0 +1,88 @@
+/*
+ * Copyright (c) 2021 MyLittleSuite
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import 'package:flutter_flavorizr/parser/models/flavors/flavor.dart';
+import 'package:flutter_flavorizr/processors/commons/empty_file_processor.dart';
+import 'package:flutter_flavorizr/processors/commons/new_file_string_processor.dart';
+import 'package:flutter_flavorizr/processors/commons/queue_processor.dart';
+import 'package:flutter_flavorizr/processors/commons/shell_processor.dart';
+import 'package:flutter_flavorizr/processors/ios/google/firebase/ios_firebase_processor.dart';
+import 'package:flutter_flavorizr/processors/ios/google/firebase/ios_firebase_script_processor.dart';
+import 'package:flutter_flavorizr/utils/ios_utils.dart' as IOSUtils;
+
+class IOSTargetsFirebaseProcessor extends QueueProcessor {
+  IOSTargetsFirebaseProcessor({
+    String process,
+    String destination,
+    String addFileScript,
+    String runnerProject,
+    String firebaseScript,
+    String generatedFirebaseScriptPath,
+    Map<String, Flavor> flavors,
+  }) : super([
+          ...flavors
+              .map(
+                (String flavorName, Flavor flavor) => MapEntry(
+                  flavorName,
+                  flavor.ios?.firebase != null
+                      ? IOSFirebaseProcessor(
+                          flavor.ios?.firebase?.config,
+                          destination,
+                          flavorName,
+                        )
+                      : null,
+                ),
+              )
+              .values
+              .where((processor) => processor != null),
+          NewFileStringProcessor(
+            '$destination/GoogleService-Info.plist',
+            EmptyFileProcessor(),
+          ),
+          ShellProcessor(
+            process,
+            [
+              addFileScript,
+              runnerProject,
+              IOSUtils.flatPath('$destination/GoogleService-Info.plist'),
+            ],
+          ),
+          NewFileStringProcessor(
+            generatedFirebaseScriptPath,
+            IOSFirebaseScriptProcessor(flavors.keys),
+          ),
+          ShellProcessor(
+            process,
+            [
+              firebaseScript,
+              runnerProject,
+              generatedFirebaseScriptPath,
+            ],
+          ),
+        ]);
+
+  @override
+  String toString() => 'IOSTargetsFirebaseProcessor';
+}

lib/processors/processor.dart
@@ -198,9 +198,14 @@ class Processor extends AbstractProcessor<void> {
 
       // Google
       'google:firebase': FirebaseProcessor(
-        K.androidSrcPath,
-        K.iOSRunnerPath,
-        _pubspec.flavorizr.flavors,
+        process: 'ruby',
+        androidDestination: K.androidSrcPath,
+        iosDestination: K.iOSRunnerPath,
+        addFileScript: K.tempiOSAddFileScriptPath,
+        runnerProject: K.iOSRunnerProjectPath,
+        firebaseScript: K.tempiOSAddFirebaseBuildPhaseScriptPath,
+        generatedFirebaseScriptPath: K.tempiOSFirebaseScriptPath,
+        flavors: _pubspec.flavorizr.flavors,
       ),
 
       // IDE

lib/utils/constants.dart
@@ -78,6 +78,9 @@ class K {
   static String tempiOSLaunchScreenPath =
       '$tempiOSPath/LaunchScreen.storyboard';
 
+  static String tempiOSFirebaseScriptPath =
+      '$tempiOSPath/firebaseScript.sh';
+
   static String tempScriptsPath = '$tempPath/scripts';
 
   static String tempiOSScriptsPath = '$tempScriptsPath/ios';
@@ -86,6 +89,8 @@ class K {
       '$tempiOSScriptsPath/create_scheme.rb';
 
   static String tempiOSAddFileScriptPath = '$tempiOSScriptsPath/add_file.rb';
+
+  static String tempiOSAddFirebaseBuildPhaseScriptPath = '$tempiOSScriptsPath/add_firebase_build_phase.rb';
 
   static String tempiOSAddBuildConfigurationScriptPath =
       '$tempiOSScriptsPath/add_build_configuration.rb';

test_resources/ios/ios_firebase_script_processor_test/multiple_flavors.sh
@@ -1,0 +1,7 @@
+if [ "$CONFIGURATION" == "Debug-apple" ] || [ "$CONFIGURATION" == "Release-apple" ]; then
+  cp Runner/apple/GoogleService-Info.plist Runner/GoogleService-Info.plist
+elif [ "$CONFIGURATION" == "Debug-banana" ] || [ "$CONFIGURATION" == "Release-banana" ]; then
+  cp Runner/banana/GoogleService-Info.plist Runner/GoogleService-Info.plist
+elif [ "$CONFIGURATION" == "Debug-pear" ] || [ "$CONFIGURATION" == "Release-pear" ]; then
+  cp Runner/pear/GoogleService-Info.plist Runner/GoogleService-Info.plist
+fi

test_resources/ios/ios_firebase_script_processor_test/single_flavor.sh
@@ -1,0 +1,3 @@
+if [ "$CONFIGURATION" == "Debug-apple" ] || [ "$CONFIGURATION" == "Release-apple" ]; then
+  cp Runner/apple/GoogleService-Info.plist Runner/GoogleService-Info.plist
+fi

test/processors/ios/google/firebase/ios_firebase_script_processor_test.dart
@@ -1,0 +1,61 @@
+/*
+ * Copyright (c) 2021 MyLittleSuite
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import 'dart:io';
+
+import 'package:flutter_flavorizr/processors/ios/google/firebase/ios_firebase_script_processor.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../test_utils.dart';
+
+void main() {
+  test('Test IOSFirebaseScriptProcessor with a single flavor', () {
+    String matcher = File(
+            '../test_resources/ios/ios_firebase_script_processor_test/single_flavor.sh')
+        .readAsStringSync();
+
+    IOSFirebaseScriptProcessor processor = IOSFirebaseScriptProcessor(['apple']);
+    String actual = processor.execute();
+
+    actual = TestUtils.stripEndOfLines(actual);
+    matcher = TestUtils.stripEndOfLines(matcher);
+
+    expect(actual, matcher);
+  });
+
+  test('Test IOSFirebaseScriptProcessor with multiple flavors', () {
+    String matcher = File(
+        '../test_resources/ios/ios_firebase_script_processor_test/multiple_flavors.sh')
+        .readAsStringSync();
+
+    IOSFirebaseScriptProcessor processor = IOSFirebaseScriptProcessor(['apple', 'banana', 'pear']);
+    String actual = processor.execute();
+
+    actual = TestUtils.stripEndOfLines(actual);
+    matcher = TestUtils.stripEndOfLines(matcher);
+
+    expect(actual, matcher);
+  });
+}