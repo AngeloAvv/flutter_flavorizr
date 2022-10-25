4edf41752b1dc832bfffb39412c0ddd432b0d8eb
Angelo Cassano <angelo.cassano@mylittlesuite.it>
2022-10-25 11:42:04 +0200

    Release 2.1.5

--- lib/parser/models/flavorizr.dart
+++ lib/parser/models/flavorizr.dart
@@ -43,7 +43,7 @@ class Flavorizr {
 
   @JsonKey(
       defaultValue:
-          'https://github.com/AngeloAvv/flutter_flavorizr/releases/download/v2.1.4/assets.zip')
+          'https://github.com/AngeloAvv/flutter_flavorizr/releases/download/v2.1.5/assets.zip')
   final String assetsUrl;
 
   @JsonKey()
