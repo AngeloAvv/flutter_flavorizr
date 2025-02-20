94d580086db6c36b44353c2a8f66d9e79e4aa3a2
wjlee611 <wjlee611@gmail.com>
2025-02-20 18:53:29 +0900

    Update example

--- example/lib/pages/my_home_page.dart
+++ example/lib/pages/my_home_page.dart
@@ -2,17 +2,13 @@ import 'package:flutter/material.dart';
 import '../flavors.dart';
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(F.title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello ${F.title}',
-        ),
-      ),
+      appBar: AppBar(title: Text(F.title)),
+      body: Center(child: Text('Hello ${F.title}')),
     );
   }
 }
