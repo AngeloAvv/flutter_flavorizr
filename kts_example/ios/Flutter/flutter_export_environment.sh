9c6254b514613ab39e29475849addee5734758af
wjlee611 <wjlee611@gmail.com>
2025-03-05 11:08:03 +0900

    Update examples

--- kts_example/ios/Flutter/flutter_export_environment.sh
+++ kts_example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/wjlee/fvm/versions/3.29.0"
-export "FLUTTER_APPLICATION_PATH=/Users/wjlee/kts_example"
+export "FLUTTER_APPLICATION_PATH=/Users/wjlee/Desktop/kts_example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
